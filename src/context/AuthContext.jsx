44fb4b164818ec870926e7c771e132b4a43a5ea4
kkydv <kkydv4546@gmail.com>
2026-01-17 13:56:38 +0530

    changed logo and brand name

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect, useContext } from "react";
+import { createContext, useState, useEffect, useContext } from "react";
 import { checkMe, logout as apiLogout } from "../api";
 
 const AuthContext = createContext();
